d7f63fcc70ef33e5a5e93a5c97e23b4eec2cbb13
1342470 <1342470@stu.ucenmanchester.ac.uk>
2021-10-26 22:38:09 +0100

    added new instance varibles for the newly added functions. added fuction for the user to input how many days it will take for the job to be compleated, In addtion to also adding to final section where the programme will output all the data and tally up the price of the job.

--- Paint Calc/Paint Calc.cpp
+++ Paint Calc/Paint Calc.cpp
@@ -1,6 +1,7 @@
 using namespace std;
 #include <iostream>
 bool correctPaint;
+bool correctDays;
 bool correctChild;
 int N;
 float P = 0.004;
@@ -10,14 +11,46 @@ float W = 1.2;
 int Nd;
 string userPaint;
 string numChild;
+string numOfDays;
 
 
+void finalCol() {
+	cout << "Okay to just to be clear we need to paint " << S << " square meters of area and their are " << Nc << " children within the area and finally it will take " << Nd << " days" << endl;
+	N = ((P * Nc * S) + W) * (1 + 1 / Nd);
+	cout << "your estmate paint required from the data given is " << N << endl;
+	cout << "from that the number of gallons the price of your paint will be " << N * 83.54  << " Britsh pound sterling" << endl;
 
+}
+	
+
+void numDays() {
+	while (correctDays == false) {
+		cout << "how many days it will take by please enter below " << endl;
+		//cout << "test to see if correctly passed  " << S << endl;
+		//cout << "test to see if correctly passed  " << Nc << endl;
+		cin >> Nd;
+		cout << "okay so it will take " << Nd << " days correct? please type either no or No if this incorrect and Yes or yes if this is correct" << endl;
+		cin >> numOfDays;
+		if (numOfDays == "No" || numOfDays == "no") {
+			correctDays = false;
+		}
+
+		else if (numOfDays == "Yes" || numOfDays == "yes") {
+			correctDays = true;
+			finalCol();
+		}
+		else {
+			cout << "please enter a valid responce" << endl;
+			correctDays = false;
+
+		};
+	}
+}
 
 void childCol() {
-	cout << "Thanks now we need to know how many children will be near by please enter below " << endl;
-	while (correctChild != true) {
-		cout << "test to see if correctly passed  " << S << endl;
+	while (correctChild == false) {
+		cout << "How many children will be near by please enter below " << endl;
+		//cout << "test to see if correctly passed  " << S << endl;
 		cin >> Nc;
 		cout << "right so their are " << Nc << " children near by" << endl;
 		cout << "okay so that's " << Nc << " children correct? please type either no or No if this incorrect and Yes or yes if this is correct" << endl;
@@ -31,17 +64,14 @@ void childCol() {
 		}
 		else {
 			cout << "please enter a valid responce" << endl;
-
+			correctChild = false;
 		};
 	}
 }
 
-void numDays() {
-
-}
 
 
-	
+
 
 void main() {
 	cout << "Welcome to the paint caluclator" << endl;
