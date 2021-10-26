dae2a1fc7b4b9f485501a6209dad342c977f7c8c
1342470 <1342470@stu.ucenmanchester.ac.uk>
2021-10-26 17:49:25 +0100

    Add project files.

---
+++ Paint Calc/Paint Calc.cpp
@@ -0,0 +1,19 @@
+using namespace std;
+#include <iostream>
+
+int main()
+{
+
+		int N;
+		float P = 0.004;
+		int Nc;
+		float S;
+		float W = 1.2;
+		int Nd;
+
+		cout << "Welcome to the paint caluclator" << endl;
+
+		return 0;
+	
+}
+
