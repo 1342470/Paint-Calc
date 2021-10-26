27e15bdabae537b333d28d61d66c21b4b9d35227
1342470 <1342470@stu.ucenmanchester.ac.uk>
2021-10-26 22:07:23 +0100

    amended how inputed data to the iostream as well as added a new function which ill allow the user to input how many children are near by.

--- Paint Calc/Paint Calc.cpp
+++ Paint Calc/Paint Calc.cpp
@@ -1,45 +1,70 @@
 using namespace std;
 #include <iostream>
+bool correctPaint;
+bool correctChild;
+int N;
+float P = 0.004;
+int Nc;
+float S;
+float W = 1.2;
+int Nd;
+string userPaint;
+string numChild;
 
-int main()
-{
 
-		int N;
-		float P = 0.004;
-		int Nc;
-		float S;
-		float W = 1.2;
-		int Nd;
-		bool correctPaint;
-		string userPaint;
 
-		cout << "Welcome to the paint caluclator" << endl;
-		cout << "please enter the area in which we will need to paint" << endl;
-		cin >> S;
-		cout << "okay so that's " << S << " square meters we need to paint correct? please type either no, No, n or N if this incorrect or Yes,yes,Y or y if this is correct" << endl;
-		cin >> userPaint;
 
-		if (userPaint == "No" || "n" || "no" || "N"){
-			correctPaint = false;
-		}
-		else if (userPaint == "Yes" || "y" || "yes" || "Y") {
-			correctPaint = true;
-		}
-
-		while(correctPaint = false) {
-			correctPaint = false;
-			cout << "please enter the area in which we will need to paint" << endl;
-			cin >> S;
-			cout << "okay so that's " << S << " square meters we need to paint correct?" << endl;
-		}
-		cout << "Thanks now we need to know how many children will be near by please enter below " << endl;
+void childCol() {
+	cout << "Thanks now we need to know how many children will be near by please enter below " << endl;
+	while (correctChild != true) {
+		cout << "test to see if correctly passed  " << S << endl;
 		cin >> Nc;
 		cout << "right so their are " << Nc << " children near by" << endl;
+		cout << "okay so that's " << Nc << " children correct? please type either no or No if this incorrect and Yes or yes if this is correct" << endl;
+		cin >> numChild;
+		if (numChild == "No" || numChild == "no") {
+			correctChild = false;
+		}
+		else if (numChild == "Yes" || numChild == "yes" ) {
+			correctChild = true;
+			numDays();
+		}
+		else {
+			cout << "please enter a valid responce" << endl;
 
-
-
-		return 0;
-	
+		};
+	}
 }
 
+void numDays() {
+
+}
+
+
 	
+
+void main() {
+	cout << "Welcome to the paint caluclator" << endl;
+	 while (correctPaint == false){
+		cout << "please enter the area in which we will need to paint" << endl;
+		cin >> S;
+		cout << "okay so that's " << S << " square meters we need to paint correct? please type either No or no if this incorrect and Yes or yes if this is correct" << endl;
+		cin >> userPaint;
+		if (userPaint == "No" || userPaint == "no") {
+			 correctPaint = false;
+		 }
+			else if (userPaint == "Yes" || userPaint == "yes" ) {
+			correctPaint = true;
+			childCol();
+		}
+		 else {
+			cout << "please enter a valid responce" << endl;
+			correctPaint = false;
+
+		};
+	}
+	 
+}
+		
+
+
